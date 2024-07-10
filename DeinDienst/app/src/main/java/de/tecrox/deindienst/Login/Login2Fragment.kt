b8f8967f320851d362f59f0fcf901caaf15eab57
Furkan Günes <97745699+furkang25@users.noreply.github.com>
2024-07-10 16:05:12 +0200

    v2.9

--- DeinDienst/app/src/main/java/de/tecrox/deindienst/Login/Login2Fragment.kt
+++ DeinDienst/app/src/main/java/de/tecrox/deindienst/Login/Login2Fragment.kt
@@ -4,11 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import de.tecrox.deindienst.R
+//import com.google.firebase.auth.FirebaseAuth
 
 class Login2Fragment : Fragment() {
+
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+    private lateinit var registerButton: Button
+    //private lateinit var mAuth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +25,12 @@ class Login2Fragment : Fragment() {
     ): View? {
         // Inflate das Layout für dieses Fragment
         val view = inflater.inflate(R.layout.fragment_login2, container, false)
+        val view2 = inflater.inflate(R.layout.fragment_register, container, false)
+
+        emailEditText = view.findViewById(R.id.email)
+        passwordEditText = view.findViewById(R.id.password)
+        loginButton = view.findViewById(R.id.loginButton)
+        registerButton = view2.findViewById(R.id.registerButton)
 
         // Finde den ImageButton und setze einen OnClickListener
         val backButton: ImageButton = view.findViewById(R.id.back_button_login2)
@@ -28,3 +43,85 @@ class Login2Fragment : Fragment() {
     }
 
 }
+
+/*
+
+class LoginFragment : Fragment() {
+
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+    private lateinit var registerButton: Button
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_login2, container, false)
+        val view2 = inflater.inflate(R.layout.fragment_register, container, false)
+
+        emailEditText = view.findViewById(R.id.email)
+        passwordEditText = view.findViewById(R.id.password)
+        loginButton = view.findViewById(R.id.loginButton)
+        registerButton = view2.findViewById(R.id.registerButton)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        loginButton.setOnClickListener {
+            loginUser()
+        }
+
+        registerButton.setOnClickListener {
+            registerUser()
+        }
+
+        return view
+    }
+
+    private fun loginUser() {
+        val email = emailEditText.text.toString().trim()
+        val password = passwordEditText.text.toString().trim()
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(activity, "Please fill all fields", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    val user: FirebaseUser? = mAuth.currentUser
+                    Toast.makeText(activity, "Login successful", Toast.LENGTH_SHORT).show()
+                    // Weiterleitung zur nächsten Activity
+                } else {
+                    Toast.makeText(activity, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun registerUser() {
+        val email = emailEditText.text.toString().trim()
+        val password = passwordEditText.text.toString().trim()
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(activity, "Please fill all fields", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    val user: FirebaseUser? = mAuth.currentUser
+                    Toast.makeText(activity, "Registration successful", Toast.LENGTH_SHORT).show()
+                    // Weiterleitung zur nächsten Activity
+                } else {
+                    Toast.makeText(activity, "Registration failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
+
+
+ */
--- DeinDienst/app/src/main/java/de/tecrox/deindienst/Login/LoginFragment.kt
+++ DeinDienst/app/src/main/java/de/tecrox/deindienst/Login/LoginFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -58,86 +59,3 @@ class LoginFragment : Fragment() {
         (activity as? MainActivity)?.showBottomNav()
     }
 }
-
-
-/*
-
-class LoginFragment : Fragment() {
-
-    private lateinit var emailEditText: EditText
-    private lateinit var passwordEditText: EditText
-    private lateinit var loginButton: Button
-    private lateinit var registerButton: Button
-    private lateinit var mAuth: FirebaseAuth
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_login2, container, false)
-        val view2 = inflater.inflate(R.layout.fragment_register, container, false)
-
-        emailEditText = view.findViewById(R.id.email)
-        passwordEditText = view.findViewById(R.id.password)
-        loginButton = view.findViewById(R.id.loginButton)
-        registerButton = view2.findViewById(R.id.registerButton)
-
-        mAuth = FirebaseAuth.getInstance()
-
-        loginButton.setOnClickListener {
-            loginUser()
-        }
-
-        registerButton.setOnClickListener {
-            registerUser()
-        }
-
-        return view
-    }
-
-    private fun loginUser() {
-        val email = emailEditText.text.toString().trim()
-        val password = passwordEditText.text.toString().trim()
-
-        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
-            Toast.makeText(activity, "Please fill all fields", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        mAuth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(requireActivity()) { task ->
-                if (task.isSuccessful) {
-                    val user: FirebaseUser? = mAuth.currentUser
-                    Toast.makeText(activity, "Login successful", Toast.LENGTH_SHORT).show()
-                    // Weiterleitung zur nächsten Activity
-                } else {
-                    Toast.makeText(activity, "Authentication failed.", Toast.LENGTH_SHORT).show()
-                }
-            }
-    }
-
-    private fun registerUser() {
-        val email = emailEditText.text.toString().trim()
-        val password = passwordEditText.text.toString().trim()
-
-        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
-            Toast.makeText(activity, "Please fill all fields", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        mAuth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(requireActivity()) { task ->
-                if (task.isSuccessful) {
-                    val user: FirebaseUser? = mAuth.currentUser
-                    Toast.makeText(activity, "Registration successful", Toast.LENGTH_SHORT).show()
-                    // Weiterleitung zur nächsten Activity
-                } else {
-                    Toast.makeText(activity, "Registration failed.", Toast.LENGTH_SHORT).show()
-                }
-            }
-    }
-}
-
-
- */
