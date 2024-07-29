378242a05663df6a74cb0f2b83a288f14d58ea8c
Furkan Günes <97745699+furkang25@users.noreply.github.com>
2024-07-29 13:19:50 +0200

    v3.8

--- DeinDienst/app/src/main/java/de/tecrox/deindienst/MyApplication.kt
+++ DeinDienst/app/src/main/java/de/tecrox/deindienst/MyApplication.kt
@@ -1,12 +1,33 @@
-// MyApplication.kt
 package de.tecrox.deindienst
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import com.google.firebase.FirebaseApp
 
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         FirebaseApp.initializeApp(this)
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        // Only create notification channels on Android Oreo and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = getString(R.string.default_notification_channel_id)
+            val channelName = getString(R.string.default_notification_channel_name)
+            val channelDescription = getString(R.string.default_notification_channel_description)
+
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, channelName, importance).apply {
+                description = channelDescription
+            }
+
+            val notificationManager: NotificationManager =
+                getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
